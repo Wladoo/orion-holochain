29fcffb0474f08feb57657141adde4e3bae793a2
alex maslakoff <alex@serendipia.email>
2019-05-18 12:24:15 -0300

    wip

--- test/index.js
+++ test/index.js
@@ -11,17 +11,12 @@ scenario.runTape("create broker", async (t, { alice }) => {
     const res1 = alice.call("orion_project1", "create_broker", {"name": "broker1"});
     var {Ok: addr} = res1;
     t.deepEqual(res1.Ok, addr);
-    // t.deepEqual(res1.Ok, res1);
-
-    // t.deepEqual("QmYfmS8M4EZRkpkNqyAtCgPaMjguAnbwupvRfpASYo4j9k", addr);
-    // t.deepEqual(, addr);
     t.ok(res1.Ok !== undefined && res1.Err === undefined);
 });
 
 scenario.runTape("create order", async (t, { alice }) => {
     //todo: why will it pass?
     const res1 = alice.call("orion_project1", "initialize_order", {
-      "name": "ord1",
       "base_asset_code": "EUR",
       "quoted_asset_code": "GBP",
       "direction": "Buy",
@@ -33,7 +28,9 @@ scenario.runTape("create order", async (t, { alice }) => {
     t.deepEqual(res1.Ok, addr1);
     t.ok(res1.Ok !== undefined);
     t.ok(res1.Err === undefined);
-    t.equal(res1, "aabbcc");
+
+    //todo: for debugging
+    t.equal(res1, "aabbcc"); 
 
 
     // todo
